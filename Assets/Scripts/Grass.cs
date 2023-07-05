9caf42560b3524677457173a2c881c8c1c478d08
Chu Hai <wench@myumanitoba.ca>
2023-07-05 16:18:24 -0500

    Clean Up

---
+++ Assets/Scripts/Grass.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using static System.Runtime.InteropServices.Marshal;
+using UnityEngine;
+using UnityEngine.TerrainUtils;
+
+public class Grass : MonoBehaviour
+{
+    
+    [SerializeField] private Mesh grassMesh;
+    [SerializeField] private Material grassMaterial;
+    [SerializeField] private Texture heightMap;
+    [SerializeField] private Material terrainMat;
+    [SerializeField] private float heightOffset;
+   
+
+    [Header("Grass Param")]
+    [SerializeField] private int fillSize;
+    [SerializeField] private float grassDensity;
+
+
+    private struct GrassData
+    {
+        private Vector4 position;
+        private Vector2 uv;
+
+        public Vector4 Position { get => position; set => position = value; }
+        public Vector2 UV { get => uv; set => uv = value; }
+    }
+
+    [SerializeField] private ComputeShader grassInit;
+    private ComputeBuffer grassPosition;
+
+    private void Start()
+    {
+        grassPosition = new ComputeBuffer(fillSize * fillSize, SizeOf(typeof(GrassData)));
+        Debug.Log(SizeOf(typeof(GrassData)));
+        grassInit.SetInt("_FillSize", fillSize);
+        grassInit.SetFloat("_Density", grassDensity);
+        grassInit.SetTexture(0, "_HeightMap", heightMap);
+        grassInit.SetFloat("_DisplacementStrength", terrainMat.GetFloat("_DisplacementStrength"));
+        grassInit.SetFloat("_Offset", heightOffset);
+        grassInit.SetBuffer(0,"_Position", grassPosition);
+        grassInit.Dispatch(0, Mathf.CeilToInt(fillSize / 8.0f), Mathf.CeilToInt(fillSize / 8.0f), 1);
+        grassMaterial.SetBuffer("_Position", grassPosition);
+
+        //GrassData[] positions = new GrassData[fillSize * fillSize];
+        //grassPosition.GetData(positions);
+    }
+    void Update()
+    {
+        grassMaterial.SetBuffer("_Position", grassPosition);
+        grassMaterial.SetFloat("_Rotation", 0.0f);
+        Graphics.DrawMeshInstancedProcedural(grassMesh, 0, grassMaterial, new Bounds(Vector3.zero, new Vector3(-500.0f, 200.0f, 500.0f)), grassPosition.count);
+
+        Material grass2 = new Material(grassMaterial);
+        grass2.SetBuffer("_Position", grassPosition);
+        grass2.SetFloat("_Rotation", 45.0f);
+        Graphics.DrawMeshInstancedProcedural(grassMesh, 0, grass2, new Bounds(Vector3.zero, new Vector3(-500.0f, 200.0f, 500.0f)), grassPosition.count);
+
+        Material grass3 = new Material(grassMaterial);
+        grass3.SetBuffer("_Position", grassPosition);
+        grass3.SetFloat("_Rotation", 135.0f);
+        Graphics.DrawMeshInstancedProcedural(grassMesh, 0, grass3, new Bounds(Vector3.zero, new Vector3(-500.0f, 200.0f, 500.0f)), grassPosition.count);
+
+    }
+    void OnDisable()
+    {
+        grassPosition.Release();
+    }
+}
---
+++ Assets/Shader/Grass.shader
@@ -0,0 +1,77 @@
+Shader "Unlit/Grass"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+            #include "UnityPBSLighting.cginc"
+            #include "AutoLight.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            StructuredBuffer<float4> _Position;
+            float _Rotation;
+
+            float4 RotateAroundYInDegrees(float4 vertex, float degrees) {
+                float alpha = degrees * UNITY_PI / 180.0;
+                float sina, cosa;
+                sincos(alpha, sina, cosa);
+                float2x2 m = float2x2(cosa, -sina, sina, cosa);
+                return float4(mul(m, vertex.xz), vertex.yw).xzyw;
+            }
+
+            v2f vert (appdata v, uint instanceID : SV_INSTANCEID)
+            {
+                v2f o;
+                float4 pos = _Position[instanceID];
+                float3 localPos = RotateAroundYInDegrees(v.vertex, _Rotation).xyz;
+                float4 worldPos = float4(pos.xyz + localPos,1.0f);
+
+                //worldPos.y *= _Position[instanceID].w;
+                o.vertex = UnityObjectToClipPos(worldPos);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+                clip(-(0.5 - col.a));
+                //Fix lighting
+                float3 lightDir = _WorldSpaceLightPos0.xyz;
+                float ndotl = DotClamped(lightDir, normalize(float3(0, 1, 0)));
+
+                return col * ndotl;
+            }
+            ENDCG
+        }
+    }
+}
